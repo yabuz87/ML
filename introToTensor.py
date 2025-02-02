6c7f3460c271bd2a6e32249ac1e82b680157f05e
Yabets solomon <jabezsolomonz@gmail.com>
2025-02-02 14:08:32 +0300

    added some notes as a comment to from the tutorial

--- introToTensor.py
+++ introToTensor.py
@@ -6,3 +6,11 @@ tensor2=tf.reshape(tensor1,[2,3,1])
 print(tensor2)
 tensor3=tf.reshape(tensor2,[3,-1])
 print(tensor3)
+# there are different types of tensor
+# variable
+# constant
+# placeholder
+# sparseTensor
+# all the aboves are types of tensor from those all are immutable except variable 
+# immutable means constant(unchaged once it's written)
+# and variable is mutable
